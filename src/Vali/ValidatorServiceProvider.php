db05ee94b573cae7e068389ff241bf1b74ea3c9c
yutakakinjyo <yutakakinjyo@gmail.com>
2017-06-30 20:08:30 +0900

    call method

--- src/Vali/ValidatorServiceProvider.php
+++ src/Vali/ValidatorServiceProvider.php
@@ -12,8 +12,8 @@ class ValidatorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-	$this->app['validator']->resolver(function($translator, $data, $rules, $messages, $customAttributes) {
-   	    return new CustomValidator($translator, $data, $rules, $messages, $customAttributes);
+	/Validator::resolver(function($translator, $data, $rules, $messages) {
+   	    return new CustomValidator($translator, $data, $rules, $messages);
 	});
     }
 

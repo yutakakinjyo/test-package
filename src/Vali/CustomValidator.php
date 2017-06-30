103b94e842a50c7d8517cb95f12e3ac51f6568a0
yutakakinjyo <yutakakinjyo@gmail.com>
2017-06-30 19:13:19 +0900

    add CusotmValidator

---
+++ src/Vali/CustomValidator.php
@@ -0,0 +1,14 @@
+class CustomValidator extends \Illuminate\Validation\Validator
+{
+
+    /**
+     * @param $attribute
+     * @param $value
+     * @param $parameters
+     * @return int
+     */
+    public function validateJpZipCode($attribute, $value, $parameters)
+    {
+        return preg_match("/^[0-9\-]{7,8}+$/i", $value);
+    }
+}
---
+++ src/Vali/ValidatorServiceProvider.php
@@ -0,0 +1,25 @@
+namespace Vali;
+
+use Illuminate\Support\ServiceProvider;
+
+class ValidatorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+	\Validator::resolver(function($translator, $data, $rules, $messages) {
+   	    return new CustomValidator($translator, $data, $rules, $messages);
+	});
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

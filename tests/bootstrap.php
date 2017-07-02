3742c5bfb819004843642384a8bbac59f96883e2
yutakakinjyo <yutakakinjyo@gmail.com>
2017-07-02 22:16:51 +0900

    add tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+// Composer has been run?
+if (! file_exists(__DIR__.'/../composer.lock')) {
+    die("You must do a 'composer install' before running the tests.");
+}
+// Autoloader
+require __DIR__.'/../vendor/autoload.php';
